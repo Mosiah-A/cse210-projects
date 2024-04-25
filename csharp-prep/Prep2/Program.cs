4039af5f655cfd659b35be4fb0babe89a1a29b8f
Mosiah-A <mosiahassuncao@gmail.com>
2024-04-25 18:21:59 -0300

    Prep2 code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade Percentage? (0-100) ");
+        string userInput = Console.ReadLine();
+        int gradeNumber = int.Parse(userInput);
+        string gradeLetter = "None";
+        if ( gradeNumber >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if ( gradeNumber >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+        
+        Console.WriteLine($"Your Grade is: {gradeLetter}");
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulation you Pass");
+        }
+        else{
+            Console.WriteLine("Sorry, you dont Pass");
+        }
     }
 }
