2199dc23e640fbb8176cb89d86ba35c81ca61444
Mosiah-A <mosiahassuncao@gmail.com>
2024-04-25 17:48:21 -0300

    Prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //first name input
+        Console.Write("what is your first name? ");
+        string fname = Console.ReadLine();
+        
+        //last name input
+        Console.Write("what is your last name? ");
+        string lname = Console.ReadLine();
+
+        //The output
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+
     }
 }
