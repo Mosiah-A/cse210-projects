2056fbce9365a01dea407ac662350eff37c631d9
Mosiah-A <mosiahassuncao@gmail.com>
2024-04-25 21:19:43 -0300

    Guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
-        Console.Write("What is the magic number? ");
-        string userInput = Console.ReadLine();
-        int answer = int.Parse(userInput);
-        while (answer != number)
+        string repet = "yes";
+        while ( repet == "yes")
         {
-            if (answer < number)
+            // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            Console.Write("What is the magic number? ");
+            string userInput = Console.ReadLine();
+            int answer = int.Parse(userInput);
+            while (answer != number)
             {
-                Console.WriteLine("Higher");
-            }
-            else if(answer > number)
-            {
-                Console.WriteLine("Lower");
+                if (answer < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if(answer > number)
+                {
+                    Console.WriteLine("Lower");
 
+                }
+                Console.Write("What is your guess? ");
+                userInput = Console.ReadLine();
+                answer = int.Parse(userInput);
             }
-            Console.Write("What is your guess?");
-            userInput = Console.ReadLine();
-            answer = int.Parse(userInput);
+            Console.WriteLine($" You guessed it! IT'S {number} ");
+            
+            Console.WriteLine("Do you want to play again? (yes / no)");
+            repet = Console.ReadLine();
         }
-        Console.WriteLine($" You guessed it! IT'S {number} ");
     }
 }
