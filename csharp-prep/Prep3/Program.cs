dd3b3cbd1d9e738a8f76419b7203d24b4915a8ed
Mosiah-A <mosiahassuncao@gmail.com>
2024-04-25 21:03:27 -0300

    Gess program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int answer = int.Parse(userInput);
+        while (answer != number)
+        {
+            if (answer < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(answer > number)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            Console.Write("What is your guess?");
+            userInput = Console.ReadLine();
+            answer = int.Parse(userInput);
+        }
+        Console.WriteLine($" You guessed it! IT'S {number} ");
     }
 }
